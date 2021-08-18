bdf4e0d80a811f8bb697f1a0858c5ac63a4a32bf
David Jokinen <jokinen.d@gmail.com>
2021-08-18 00:12:11 -0400

    UI theme test

--- game/ui/pages/pause/index.js
+++ game/ui/pages/pause/index.js
@@ -20,6 +20,13 @@ const useStyles = makeStyles((theme) => ({
     margin: 'auto',
     marginBottom: theme.spacing(2),
     maxWidth: 500,
+    borderImageSlice: 2,
+    borderImageWidth: 2,
+    borderImageRepeat: 'stretch',
+    borderImageSource: `url('data:image/svg+xml;utf8,<?xml version="1.0" encoding="UTF-8" ?><svg version="1.1" width="5" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2 1 h1 v1 h-1 z M1 2 h1 v1 h-1 z M3 2 h1 v1 h-1 z M2 3 h1 v1 h-1 z" fill="rgb(33,37,41)" /></svg>')`,
+    borderStyle: 'solid',
+    borderWidth: 4,
+    boxSizing: 'border-box',
   },
   header: {
     textAlign: 'center',
@@ -29,6 +36,7 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(2),
     fontFamily: "'Free Pixel', sans-serif",
   },
+
 }));
 
 export default function Pause(props) {
@@ -53,7 +61,7 @@ export default function Pause(props) {
       </Paper>
       <Paper className={classes.paper}>
         <Button variant="contained" fullWidth={true} className={classes.button} onClick={startAction}>Back to Game</Button>
-        <Button variant="contained" fullWidth={true} className={classes.button} onClick={settingsAction}>Settings</Button>
+        {/* <Button variant="contained" fullWidth={true} className={classes.button} onClick={settingsAction}>Settings</Button> */}
         <Button variant="contained" fullWidth={true} className={classes.button} onClick={titleAction}>Exit to Title Screen</Button>
       </Paper>
     </Container>
